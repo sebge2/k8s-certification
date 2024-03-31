cf258ec53102a3dbec88b84d5c91e1ae2390181d
Sébastien Gérard <me@sgerard.be>
2024-03-31 19:12:04 +0200

    lab 9

--- cka/lab-9/cluster/init-scripts/init-cilium.sh
+++ cka/lab-9/cluster/init-scripts/init-cilium.sh
@@ -4,5 +4,5 @@
 helm repo add cilium https://helm.cilium.io/
 helm repo update
 
-helm template cilium cilium/cilium --version 1.14.1 --namespace kube-system --set routing-mode=native --set ipv4-native-routing-cidr=192.168.128.0/24 > /home/ubuntu/cilium.yaml
+helm template cilium cilium/cilium --version 1.14.1 --namespace kube-system --set routing-mode=native --set ipv4-native-routing-cidr=192.168.0.0/16 > /home/ubuntu/cilium.yaml
 kubectl --kubeconfig /home/ubuntu/.kube/config apply -f /home/ubuntu/cilium.yaml
