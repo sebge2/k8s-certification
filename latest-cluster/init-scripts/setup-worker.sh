f70ad40b29bb1836de63ef7fc3c560b4ab9fb85c
Sébastien Gérard <me@sgerard.be>
2024-04-14 07:46:58 +0200

    add latest cluster

--- latest-cluster/init-scripts/setup-worker.sh
+++ latest-cluster/init-scripts/setup-worker.sh
@@ -5,6 +5,7 @@ mkdir -p /home/ubuntu/init-scripts
 sleep 120
 
 mv /home/ubuntu/init-*.sh /home/ubuntu/init-scripts/
+mv /home/ubuntu/setup-*.sh /home/ubuntu/init-scripts/
 mv /home/ubuntu/join-command-helper.sh /home/ubuntu/init-scripts/
 sh -x /home/ubuntu/init-scripts/init-kube.sh >> /home/ubuntu/logs/init-kube.log 2>&1
 sh -x /home/ubuntu/init-scripts/init-containerd.sh >> /home/ubuntu/logs/init-containerd.log 2>&1
