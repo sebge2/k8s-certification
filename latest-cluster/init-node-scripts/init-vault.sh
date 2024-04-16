98e738c72705c46d19cb81cc3160e9047d7e8288
Sébastien Gérard <me@sgerard.be>
2024-04-16 20:30:58 +0200

    add latest cluster

--- latest-cluster/init-node-scripts/init-vault.sh
+++ latest-cluster/init-node-scripts/init-vault.sh
@@ -5,18 +5,13 @@
 
 VAULT_NAMESPACE="vault"
 VAULT_AUTH_PATH="kubernetes"
+SECRET_NAME="vault-token"
 
 kubectl --kubeconfig /home/ubuntu/.kube/config create ns $VAULT_NAMESPACE
 
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
-helm install vault hashicorp/vault --create-namespace --namespace $VAULT_NAMESPACE --set "global.externalVaultAddr=http://vault.sfeir.local:8200"
-
-SECRET_NAME=$(kubectl get secrets -n $VAULT_NAMESPACE --output=json | jq -r '.items[].metadata | select(.name|startswith("vault-token-")).name')
-TOKEN_REVIEW_JWT=$(kubectl get secret "$SECRET_NAME" -n $VAULT_NAMESPACE --output='go-template={{ .data.token }}' | base64 --decode)
-KUBE_CA_CERT=$(cat /etc/kubernetes/pki/apiserver.crt)
-
-ssh -o StrictHostKeyChecking=no -i /home/ubuntu/node.key "ubuntu@vault.sfeir.local" "sh /home/ubuntu/init-vault-scripts/create-kube-auth.sh \"${TOKEN_REVIEW_JWT}\" \"${KUBE_CA_CERT}\""
+helm install vault hashicorp/vault --create-namespace --namespace $VAULT_NAMESPACE --kubeconfig "/home/ubuntu/.kube/config" --set "global.externalVaultAddr=http://vault.sfeir.local:8200"
 
 kubectl --kubeconfig /home/ubuntu/.kube/config apply -f -<<EOF
 apiVersion: v1
@@ -28,6 +23,11 @@ metadata:
     kubernetes.io/service-account.name: vault
 type: kubernetes.io/service-account-token
 EOF
+
+TOKEN_REVIEW_JWT=$(kubectl get secret "$SECRET_NAME" -n $VAULT_NAMESPACE --output='go-template={{ .data.token }}' --kubeconfig /home/ubuntu/.kube/config| base64 --decode)
+KUBE_CA_CERT=$(cat /etc/kubernetes/pki/apiserver.crt)
+
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/node.key "ubuntu@vault.sfeir.local" "sh /home/ubuntu/init-vault-scripts/create-kube-auth.sh \"${TOKEN_REVIEW_JWT}\" \"${KUBE_CA_CERT}\""
 
 helm install vault-secrets-operator hashicorp/vault-secrets-operator -n vault-secrets-operator-system --create-namespace --kubeconfig "/home/ubuntu/.kube/config" -f -<<EOF
 defaultVaultConnection:
