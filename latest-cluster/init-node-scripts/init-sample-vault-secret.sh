af0dbc2bbb294f680fd3642fc963187ef7eac00c
Sébastien Gérard <me@sgerard.be>
2024-04-16 21:11:01 +0200

    add latest cluster

--- latest-cluster/init-node-scripts/init-sample-vault-secret.sh
+++ latest-cluster/init-node-scripts/init-sample-vault-secret.sh
@@ -12,3 +12,20 @@ sh /home/ubuntu/init-node-scripts/create-vault-secret.sh $SECRET_PATH $READ_POLI
 # Debug
 # kubectl describe vaultstaticsecrets -n my-app vault-static-secret
 # kubectl describe vaultauths -n my-app vault-static-auth
+
+# Test:
+#apiVersion: v1
+#kind: Pod
+#metadata:
+#  name: pod-with-secret
+#spec:
+#  containers:
+#    - name: nginx
+#      image: nginx:1.15.1
+#      env:
+#        #  kubectl exec -ti pod-with-secret -- env
+#        - name: MY_ENV
+#          valueFrom:
+#            secretKeyRef:
+#              name: my-secret
+#              key: password
