3efc7934719cddcbc60171595fb0317effa8df9e
Sébastien Gérard <me@sgerard.be>
2024-04-16 09:31:41 +0200

    add latest cluster

--- latest-cluster/init-vault-scripts/create-kube-auth.sh
+++ latest-cluster/init-vault-scripts/create-kube-auth.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-TOKEN_REVIEW_JWT=$0
-KUBE_CA_CERT=$1
+TOKEN_REVIEW_JWT="$1"
+KUBE_CA_CERT="$2"
 KUBE_CA_CERT_ISSUER=https://kubernetes.default.svc.cluster.local
 VAULT_AUTH_PATH="kubernetes"
-VAULT_ADDRESS="http://127.0.0.1:8200"
 KUBE_HOST="https://cp.sfeir.local:6443"
+VAULT_ADDRESS="http://127.0.0.1:8200"
 ROOT_TOKEN=$(grep "Initial Root Token: " < /home/ubuntu/vault.keys | cut -d ' ' -f 4)
 
 sudo vault login -address=$VAULT_ADDRESS "$ROOT_TOKEN"
+
+echo "CA CERT:"
+echo "$KUBE_CA_CERT"
 
 sudo vault auth enable -address=$VAULT_ADDRESS -path $VAULT_AUTH_PATH kubernetes
 sudo vault write -address=$VAULT_ADDRESS auth/$VAULT_AUTH_PATH/config token_reviewer_jwt="$TOKEN_REVIEW_JWT" kubernetes_host="$KUBE_HOST" kubernetes_ca_cert="$KUBE_CA_CERT" issuer="$KUBE_CA_CERT_ISSUER"
