c7e52ee50323e827470524e893bb404b32cd586e
Sébastien Gérard <me@sgerard.be>
2024-04-16 17:20:25 +0200

    add latest cluster

--- latest-cluster/init-node-scripts/init-vault.sh
+++ latest-cluster/init-node-scripts/init-vault.sh
@@ -1,70 +1,29 @@
 #!/bin/bash
 
+# https://developer.hashicorp.com/vault/tutorials/kubernetes/kubernetes-external-vault
 # https://developer.hashicorp.com/vault/tutorials/kubernetes/vault-secrets-operator
 
-APP_NAMESPACE="my-app"
-APP_ROLE="my-app-reader"
-SECRET_PATH="my-app"
+VAULT_NAMESPACE="default"
+
+kubectl --kubeconfig /home/ubuntu/.kube/config create ns $VAULT_NAMESPACE
 
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
-helm install vault-secrets-operator hashicorp/vault-secrets-operator -n vault-secrets-operator-system --create-namespace --kubeconfig "/home/ubuntu/.kube/config" -f -<<EOF
-defaultVaultConnection:
-  enabled: true
-  address: "http://vault.sfeir.local:8200"
-  skipTLSVerify: false
-controller:
-  manager:
-    clientCache:
-      persistenceModel: direct-encrypted
-      storageEncryption:
-        enabled: true
-        mount: kubernetes
-        keyName: vso-client-cache
-        transitMount: auth-role-operator-transit
-        kubernetes:
-          role: auth-role-operator
-          serviceAccount: auth-role-operator
-EOF
-
-CA_CERT=$(kubectl get cm kube-root-ca.crt -o jsonpath="{['data']['ca\.crt']}" --kubeconfig /home/ubuntu/.kube/config)
-ssh -o StrictHostKeyChecking=no -i /home/ubuntu/node.key "ubuntu@vault.sfeir.local" "sh /home/ubuntu/init-vault-scripts/bind-vault-kube.sh \"${CA_CERT}\""
-
-kubectl create ns my-app --kubeconfig /home/ubuntu/.kube/config
+helm install vault hashicorp/vault --create-namespace --namespace $VAULT_NAMESPACE --set "global.externalVaultAddr=http://vault.sfeir.local:8200"
 
 kubectl --kubeconfig /home/ubuntu/.kube/config apply -f -<<EOF
-apiVersion: secrets.hashicorp.com/v1beta1
-kind: VaultAuth
+apiVersion: v1
+kind: Secret
 metadata:
-  name: static-auth
-  namespace: $APP_NAMESPACE
-spec:
-  method: kubernetes
-  mount: kubernetes
-  kubernetes:
-    role: $APP_ROLE
-    serviceAccount: default
-    audiences:
-      - vault
+  name: vault-token-g955r
+  namespace: $VAULT_NAMESPACE
+  annotations:
+    kubernetes.io/service-account.name: vault
+type: kubernetes.io/service-account-token
 EOF
 
-kubectl --kubeconfig /home/ubuntu/.kube/config apply -f -<<EOF
-apiVersion: secrets.hashicorp.com/v1beta1
-kind: VaultStaticSecret
-metadata:
-  name: vault-kv-app
-  namespace: $APP_NAMESPACE
-spec:
-  type: kv-v2
-  mount: kvv2
-  path: $SECRET_PATH
-  destination:
-    name: secretkv
-    create: true
-  refreshAfter: 30s
-  vaultAuthRef: static-auth
-EOF
+SECRET_NAME=$(kubectl get secrets -n $VAULT_NAMESPACE --output=json | jq -r '.items[].metadata | select(.name|startswith("vault-token-")).name')
+TOKEN_REVIEW_JWT=$(kubectl get secret "$SECRET_NAME" -n $VAULT_NAMESPACE --output='go-template={{ .data.token }}' | base64 --decode)
+KUBE_CA_CERT=$(cat /etc/kubernetes/pki/apiserver.crt)
 
-# Debug
-# kubectl describe vaultstaticsecrets -n my-app vault-kv-app
-# kubectl describe vaultauths -n my-app
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/node.key "ubuntu@vault.sfeir.local" "sh /home/ubuntu/init-vault-scripts/create-kube-auth.sh \"${TOKEN_REVIEW_JWT}\" \"${KUBE_CA_CERT}\""
--- latest-cluster/init-vault-scripts/init-all.sh
+++ latest-cluster/init-vault-scripts/init-all.sh
@@ -6,6 +6,7 @@ mkdir -p /home/ubuntu/init-vault-scripts
 sleep 60
 
 mv /home/ubuntu/init-*.sh /home/ubuntu/init-vault-scripts/
-mv /home/ubuntu/bind-vault-kube.sh /home/ubuntu/init-vault-scripts/
+mv /home/ubuntu/create-kube-auth.sh /home/ubuntu/init-vault-scripts/
+mv /home/ubuntu/create-role.sh /home/ubuntu/init-vault-scripts/
 
 sh -x /home/ubuntu/init-vault-scripts/init-vault.sh >> /home/ubuntu/logs/init-vault.log 2>&1
